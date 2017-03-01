d980d114a939f4b8a48f59ccdc27da6d11b4a8d3
nishina <n.toshi.1.014@gmail.com>
2017-03-02 00:40:09 +0900

    :sparkles: Add showing search query

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,17 +1,20 @@
 class StudentsController < ApplicationController
   def index
     @q = Student.ransack(params[:q])
-    @students = @q.result.includes(:department, :subjects)
+    @departments = Department.all
+    @subjects = Subject.all
+    @students = @q.result(distinct: true).includes(:department, :subjects)
   end
 
   def search
     @q = Student.search(search_params)
-    @students = @q.result.includes(:department, :subjects)
+    @students = @q.result(distinct: true).includes(:department, :subjects)
+    @search_query = params[:q]
   end
 
   private
   def search_params
-    params.require(:q).permit(:name_cont)
+    params.require(:q).permit(:name_cont, :department_id_eq, :sex_eq, :age_gteq, :subjects_id_in => [])
   end
 
 end
